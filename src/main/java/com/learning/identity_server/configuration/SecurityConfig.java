e7c405c6f7ca8ed7fa97cc78bf3d2d8201e47bf6
huytq@ics-p.vn <huytq@ics-p.vn>
2024-08-27 14:53:46 +0700

    add swagger

--- src/main/java/com/learning/identity_server/configuration/SecurityConfig.java
+++ src/main/java/com/learning/identity_server/configuration/SecurityConfig.java
@@ -29,6 +29,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers("/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated());
 
         httpSecurity.oauth2ResourceServer(oauth2 ->
